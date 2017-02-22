3b1be679e4919ec454773c429c412320bdd8f7f5
Pete Hawkins <pete@phawk.co.uk>
2017-02-22 23:11:24 +0000

    Better execution

--- src/main.js
+++ src/main.js
@@ -3,29 +3,40 @@
 import Vue from "vue";
 import App from "./App";
 
-document.addEventListener("DOMContentLoaded", function() {
-  var data = {
-    host: "pooreffort.com",
-    apiRoot: "http://localhost:5000/",
-    token: "a40e0b9b-a8c6-4118-8667-4dea6600bcbb"
-  };
+(function() {
+  function setup() {
+    console.log("setup();");
+    var data = {
+      host: "pooreffort.com",
+      apiRoot: "http://localhost:5000/",
+      token: "a40e0b9b-a8c6-4118-8667-4dea6600bcbb"
+    };
 
-  var div = document.createElement("div");
+    var div = document.createElement("div");
 
-  document.body.appendChild(div);
+    document.body.appendChild(div);
 
-  var scriptTag = document.getElementById("void-app-bookmarklet-tag");
+    var scriptTag = document.getElementById("void-app-bookmarklet-tag");
 
-  if (scriptTag) {
-    data.host = window.location.host || window.location.href;
-    data.apiRoot = scriptTag.dataset.apiRoot;
-    data.token = scriptTag.dataset.secureToken;
+    if (scriptTag) {
+      data.host = window.location.host || window.location.href;
+      data.apiRoot = scriptTag.dataset.apiRoot;
+      data.token = scriptTag.dataset.secureToken;
+    }
+
+    /* eslint-disable no-new */
+    new Vue({
+      data: { data },
+      template: '<App :site-data="data" />',
+      components: { App }
+    }).$mount(div);
   }
 
-  /* eslint-disable no-new */
-  new Vue({
-    data: { data },
-    template: '<App :site-data="data" />',
-    components: { App }
-  }).$mount(div);
-});
+  if (document.body) {
+    console.log("document.body found");
+    setup();
+  } else {
+    console.log("waiting for DOMContentLoaded");
+    document.addEventListener("DOMContentLoaded", setup);
+  }
+}());
