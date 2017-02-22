ee93ac0c7d93df8f3721ad29062e336d9501f265
Pete Hawkins <pete@phawk.co.uk>
2017-02-22 21:09:41 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <bookmarklet></bookmarklet>
+  </div>
+</template>
+
+<script>
+import Bookmarklet from './components/Bookmarklet'
+
+export default {
+  name: 'app',
+  components: {
+    Bookmarklet
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 999999999;
+}
+</style>
---
+++ src/components/Bookmarklet.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="void-bookmarklet">
+    <span class="text"><strong>{{ title }}</strong> has been added to the void!</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bookmarklet',
+  data () {
+    return {
+      title: "Demo"
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$font-stack: -apple-system, BlinkMacSystemFont,
+  "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell",
+  "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+
+.void-bookmarklet {
+  position: fixed;
+  top: 30px;
+  left: 30px;
+  padding: 10px 20px;
+  background: #fff;
+  box-shadow: 3px 3px 10px rgba(0,0,0,0.1);
+  border-radius: 6px;
+  max-width: 60%;
+
+  .text {
+    font-family: $font-stack;
+    font-size: 19px;
+    font-weight: 300;
+    line-height: 24px;
+    color: #444;
+  }
+}
+</style>
