1ba9715eb96a5f788bd40d376b59978c4ed0af75
Pete Hawkins <pete@phawk.co.uk>
2017-05-17 23:24:44 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ if [ $# -eq 0 ]
 elif [ $1 == "production" ]
     then
         echo "Running production deploy"
-        npm run build && aws s3 cp ./dist/static s3://void-bookmarklet --recursive --profile pooreffort --acl public-read --region=eu-west-1
+        npm run build && aws s3 sync --delete --acl public-read --region=eu-west-1 --profile pooreffort ./dist/static s3://void-bookmarklet
 elif [ $1 == "local" ]
     then
         echo "Running local deploy build"
