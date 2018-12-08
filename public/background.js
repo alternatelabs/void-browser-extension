e38bf8a2bc79dc86b93202d0203c5dfc8f3e2c15
Pete Hawkins <pete@alternatelabs.co>
2018-12-08 15:35:19 +0000

    Update icon to show status of bookmark on tab change

--- public/background.js
+++ public/background.js
@@ -6,3 +6,42 @@ chrome.contextMenus.create({
     localStorage.removeItem("apiToken");
   }
 });
+
+/* On tab change update the icons */
+chrome.tabs.onActivated.addListener(function (tabInfo) {
+  chrome.tabs.get(tabInfo.tabId, function(activeTab) {
+    const parser = document.createElement("a");
+    if (typeof activeTab.url === "string") {
+      parser.href = activeTab.url;
+      console.log("Active Tab", parser.hostname);
+
+      const token = localStorage.getItem("apiToken");
+      const url = encodeURIComponent(activeTab.url);
+
+      fetch(
+        `https://api.voidapp.co/api/v1/bookmarks/exists?url=${url}`,
+        {
+          method: "GET",
+          headers: {
+            "Authorization": `Token ${token}`
+          },
+        }
+      )
+      .then(resp => {
+        if (resp.status === 204) {
+          chrome.browserAction.setIcon({
+            path: "icons/icon-38.png"
+          });
+        } else {
+          chrome.browserAction.setIcon({
+            path: "icons/icon-38-dull.png"
+          });
+        }
+      })
+      .catch(err => console.error("Error checking URL", err));
+    } else {
+      console.error("Current tab doesn't have a URL", activeTab);
+    }
+  });
+});
+
